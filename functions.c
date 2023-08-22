5bdcde72e93c01adaebeae484f67bf9d446b1fbf
brownskingirl <64357561+glohvall@users.noreply.github.com>
2023-08-22 10:42:15 +0300

    Update functions.c
    Returning a float function I thought I could alter

--- functions.c
+++ functions.c
@@ -56,5 +56,15 @@ int print_unsignedint(unsigned int val)
  * print_float- prints a float
  * @val:the number
  */
+void print_float(double val)
+{
+	int dec = 1000000;
+	int i = (int)val;
 
+	_print_int(i);
+	val = (val - i) * dec;
+	i = (int)val;
+	_putchar('.');
+	_print_int(i);
+}
 
